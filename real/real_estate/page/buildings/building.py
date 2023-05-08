6c71b3338042ce1b72db1897312ef183a3d5a911
Mainul <mainulkhan94@gmail.com>
2023-05-08 15:04:09 +0600

    feat: added workspace.

--- real/real_estate/page/buildings/building.py
+++ real/real_estate/page/buildings/building.py
@@ -3,4 +3,4 @@ import frappe
 
 @frappe.whitelist()
 def get_buildings():
-    return frappe.get_list('Real Item', fields=['name', 'notes', 'flat_details', 'gps_coordinates'],limit_page_length=0)
+    return frappe.get_list('Real Item', fields=['name', 'notes', 'flat_details', 'gps_coordinates', 'building_date'],limit_page_length=0)
